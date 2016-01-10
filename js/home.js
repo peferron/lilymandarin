dfbd041599c9b50adb32df70f7a163768c4bcb64
Pierre-Emile Ferron <peferron@gmail.com>
2016-01-10 16:39:25 +0100

    Way too much stuff for a honest commit

--- js/home.js
+++ js/home.js
@@ -15,40 +15,4 @@
 
     window.addEventListener('resize', onWindowResize);
     onWindowResize();
-
-    //
-    // Fade tagline arrow when scrolling.
-    //
-
-    function getScrollRatio() {
-        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-        var clientHeight = document.documentElement.clientHeight;
-        return scrollTop / (scrollHeight - clientHeight);
-    }
-
-    var homeTaglineArrow = document.getElementsByClassName('home__tagline__arrow')[0]
-    function updateTaglineArrow() {
-        var scrollRatio = getScrollRatio();
-
-        var heightEm = (1 - scrollRatio * 2) * 1.2;
-        var normalizedHeightEm = Math.max(0, Math.min(1.2, heightEm));
-        homeTaglineArrow.style.height = normalizedHeightEm + 'em';
-
-        var opacity = 1 - scrollRatio * 8;
-        var normalizedOpacity = Math.max(0, Math.min(1, opacity));
-        homeTaglineArrow.style.opacity = normalizedOpacity;
-    }
-    updateTaglineArrow();
-
-    var waitingForAnimationFrame = false;
-    window.addEventListener('scroll', function() {
-        if (!waitingForAnimationFrame) {
-            waitingForAnimationFrame = true;
-            requestAnimationFrame(function() {
-                updateTaglineArrow();
-                waitingForAnimationFrame = false;
-            });
-        }
-    });
 }());
